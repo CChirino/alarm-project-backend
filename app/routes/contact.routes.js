9acbcfe43d01658f077f0253a6d173696c8413be
Christopher Chirino <critijo@gmail.com>
2022-04-25 17:53:16 -0400

    contact - routes

--- app/routes/contact.routes.js
+++ app/routes/contact.routes.js
@@ -1,22 +1,22 @@
-const { authJwt } = require("../middleware");
-const contacts = require("../controllers/contact.controller");
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
-  router.post("/", contacts.create);
-  // Retrieve all Tutorials
-  router.get("/", contacts.findAll);
-  // Retrieve a single Tutorial with id
-  router.get("/:id", contacts.findOne);
-  // Update a Tutorial with id
-  router.put("/:id", contacts.update);
-  // Delete a Tutorial with id
-  router.delete("/:id", contacts.delete);
-  // Delete all Tutorials
-  router.delete("/", contacts.deleteAll);
-};
+const contact = require("../controllers/contact.controller.js");
+  module.exports = function(app) {
+    app.use(function(req, res, next) {
+      res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+      );
+      next();
+    });
+    app.post("/", contact.create);
+    // Retrieve all Contacts
+    app.get("/", contact.findAll);
+    // Retrieve a single Contacts with id
+    app.get("/:id", contact.findOne);
+    // Update a Contacts with id
+    app.put("/:id", contact.update);
+    // Delete a Contacts with id
+    app.delete("/:id", contact.delete);
+    // Delete all  Contacts
+    app.delete("/", contact.deleteAll);
+  };
+  
